216d2cc0ce5d6d0de7f359754843e948314a4f70
Svetik-K <sviatlana_kliautsova@icloud.com>
2022-12-23 14:01:16 +0100

    refactor: sliders' code

--- src/components/filters/slider.ts
+++ src/components/filters/slider.ts
@@ -1,4 +1,3 @@
-// prices slider
 const priceSliderOne = <HTMLInputElement>document.querySelector('.price-input_first');
 const priceSliderTwo = <HTMLInputElement>document.querySelector('.price-input_second');
 const priceValueOne = <HTMLSpanElement>document.querySelector('.price-slider__value-one');
@@ -7,29 +6,46 @@ const priceSliderTrack = <HTMLDivElement>document.querySelector('.price-slider__
 const priceMaxValue = priceSliderOne.max;
 let minGap = 5;
 
-priceSliderOne?.addEventListener('input', slidePricesOne);
-priceSliderTwo?.addEventListener('input', slidePricesTwo);
+priceSliderOne?.addEventListener('input', () => {
+    slidePricesOne(priceSliderOne, priceSliderTwo, minGap, priceValueOne, priceMaxValue, priceSliderTrack);
+});
+priceSliderTwo?.addEventListener('input', () => {
+    slidePricesTwo(priceSliderOne, priceSliderTwo, minGap, priceValueTwo, priceMaxValue, priceSliderTrack);
+});
 
-function slidePricesOne() {
-    if((parseInt(priceSliderTwo.value) - parseInt(priceSliderOne.value)) <= minGap) {
-        priceSliderOne.value = (parseInt(priceSliderTwo.value) - minGap).toString();
+const stockSliderOne = <HTMLInputElement>document.querySelector('.stock-input_first');
+const stockSliderTwo = <HTMLInputElement>document.querySelector('.stock-input_second');
+const stockValueOne = <HTMLSpanElement>document.querySelector('.stock-slider__value-one');
+const stockValueTwo = <HTMLSpanElement>document.querySelector('.stock-slider__value-two');
+const stockSliderTrack = <HTMLDivElement>document.querySelector('.stock-slider__track');
+const stockMaxValue = stockSliderOne.max;
+
+stockSliderOne?.addEventListener('input', () => {
+    slidePricesOne(stockSliderOne, stockSliderTwo, minGap, stockValueOne, stockMaxValue, stockSliderTrack);
+});
+stockSliderTwo?.addEventListener('input', () => {
+    slidePricesTwo(stockSliderOne, stockSliderTwo, minGap, stockValueTwo, stockMaxValue, stockSliderTrack);
+});
+
+function slidePricesOne(sliderOne: HTMLInputElement, sliderTwo: HTMLInputElement, gap: number, valueOne: HTMLSpanElement, maxValue: string, sliderTrack: HTMLDivElement) {
+    if((parseInt(sliderTwo.value) - parseInt(sliderOne.value)) <= gap) {
+        sliderOne.value = (parseInt(sliderTwo.value) - gap).toString();
     }
-    priceValueOne.textContent = priceSliderOne.value;
-    fillWithColor();
+    valueOne.textContent = sliderOne.value;
+    fillWithColor(sliderOne, sliderTwo, maxValue, sliderTrack);
 }
 
-function slidePricesTwo() {
-    if((parseInt(priceSliderTwo.value) - parseInt(priceSliderOne.value)) <= minGap) {
-        priceSliderTwo.value = (parseInt(priceSliderOne.value) + minGap).toString();
+function slidePricesTwo(sliderOne: HTMLInputElement, sliderTwo: HTMLInputElement, gap: number, valueTwo: HTMLSpanElement, maxValue: string, sliderTrack: HTMLDivElement) {
+    if((parseInt(sliderTwo.value) - parseInt(sliderOne.value)) <= gap) {
+        sliderTwo.value = (parseInt(sliderOne.value) + gap).toString();
     }
-    priceValueTwo.textContent = priceSliderTwo.value;
-    fillWithColor();
+    valueTwo.textContent = sliderTwo.value;
+    fillWithColor(sliderOne, sliderTwo, maxValue, sliderTrack);
 }
 
-function fillWithColor() {
-    let percentOne = (parseInt(priceSliderOne.value) / parseInt(priceMaxValue)) * 100;
-    let percentTwo = (parseInt(priceSliderTwo.value) / parseInt(priceMaxValue)) * 100;
-    priceSliderTrack.style.background = `linear-gradient(to right, #efefef ${percentOne}%, #333e48 ${percentOne}%, #333e48 ${percentTwo}%, #efefef ${percentTwo}%)`;
+function fillWithColor(sliderOne: HTMLInputElement, sliderTwo: HTMLInputElement, maxValue: string, sliderTrack: HTMLDivElement) {
+    let percentOne = (parseInt(sliderOne.value) / parseInt(maxValue)) * 100;
+    let percentTwo = (parseInt(sliderTwo.value) / parseInt(maxValue)) * 100;
+    sliderTrack.style.background = `linear-gradient(to right, #efefef ${percentOne}%, #333e48 ${percentOne}%, #333e48 ${percentTwo}%, #efefef ${percentTwo}%)`;
 }
 
-//stock slider
