5803d63e0896bafd2d0f0780320bf5e17e5547c8
Svetik-K <sviatlana_kliautsova@icloud.com>
2023-01-09 18:51:12 +0100

    feat: add personal details validation to modal

--- src/components/modal/modal.ts
+++ src/components/modal/modal.ts
@@ -8,6 +8,10 @@ class Modal {
         this.container.className = 'module-layer';
     }
 
+    public isNameValid() {
+        
+    }
+
     draw() {
         const formContainer = document.createElement('form');
         formContainer.className = 'form';
@@ -16,34 +20,43 @@ class Modal {
         const detailsTitle = document.createElement('h3');
         detailsTitle.className = 'form__title';
         detailsTitle.textContent = 'Personal details';
-        formContainer.append(detailsTitle);
-
-        const nameWarning = document.createElement('p'); 
-        nameWarning.className = 'name-warning';
-        nameWarning.textContent = 'Your name and surname should be no shorter than 3 symbols each';
-        formContainer.append(nameWarning);   
+        formContainer.append(detailsTitle);  
         
         const nameInput = document.createElement('input');
         nameInput.className = 'form__name';
         nameInput.type = 'text';
         nameInput.placeholder = 'Enter Your name & surname';
-        nameInput.pattern = '/[a-zA-Z]\b[a-zA-Z]/'; // под вопросом
+        nameInput.pattern = "[\w]{3,}[ ]+[\w]{3,}";
         nameInput.required = true;
         formContainer.append(nameInput);
 
-        nameInput.addEventListener('change', (e) => {
-            const target = <HTMLInputElement>e.target;
-            const numbers = '01234567890¬`!"£$%^&*()_+[]{}@:,./\\|<>#';
-            const valueItems = target.value.split(' ');
+        const nameWarning = document.createElement('p'); 
+        nameWarning.className = 'name-warning';
+        nameWarning.textContent = 'Your name and surname must not be shorter than 3 symbols each';
+        formContainer.append(nameWarning);
+        
+        let nameValid = false;
+
+        nameInput.addEventListener('input', () => {
+            const valueItems = nameInput.value.split(' ');
+            if(valueItems.length < 2) {
+                nameInput.classList.add('invalid');
+                nameWarning.classList.add('shown');
+                nameValid = false;
+                return;
+            }
+
             valueItems.forEach((item) => {
                 if(item.length < 3) {
                     nameInput.classList.add('invalid');
-                    nameWarning.style.display = 'block';
+                    nameWarning.classList.add('shown');
+                    nameValid = false;
                 } else {
+                    nameInput.classList.remove('invalid');
                     nameInput.classList.add('valid');
-                    nameWarning.style.display = 'none';
-                }
-                
+                    nameWarning.classList.remove('shown');
+                    nameValid = true;
+                }   
             })
         })
         
@@ -51,9 +64,27 @@ class Modal {
         phoneInput.className = 'form__phone';
         phoneInput.type = 'tel';
         phoneInput.placeholder = '+375 XXX XXX';
-        phoneInput.pattern = '\+?[0-9\s\-\(\)]+';
+        phoneInput.pattern = "^(?:\\+)[0-9\\s.\\/-]{9,20}$"; //работает
         phoneInput.required = true;
         formContainer.append(phoneInput);
+
+        const phoneWarning = document.createElement('p'); 
+        phoneWarning.className = 'phone-warning';
+        phoneWarning.textContent = 'Please enter valid phone number';
+        formContainer.append(phoneWarning);
+
+        let phoneValid = false;
+
+        phoneInput.addEventListener('input', () => {
+            if(phoneInput.value.length < 9) {
+                phoneWarning.classList.add('shown');
+                phoneInput.classList.add('invalid');
+                phoneValid = false;
+            }
+            phoneWarning.classList.remove('shown');
+            phoneInput.classList.add('valid');
+            phoneValid = true;
+        })
         
         const addressInput = document.createElement('input');
         addressInput.className = 'form__address';
@@ -61,6 +92,36 @@ class Modal {
         addressInput.placeholder = 'Delivery address';
         addressInput.required = true;
         formContainer.append(addressInput);
+
+        let addressValid = false;
+
+        addressInput.addEventListener('input', () => {
+            const valueItems = addressInput.value.split(' ');
+            if(valueItems.length < 3) {
+                addressInput.classList.add('invalid');
+                addressWarning.classList.add('shown');
+                addressValid = false;
+                return;
+            }
+
+            valueItems.forEach((item) => {
+                if(item.length < 5) {
+                    addressInput.classList.add('invalid');
+                    addressWarning.classList.add('shown');
+                    addressValid = false;
+                } else {
+                    addressInput.classList.remove('invalid');
+                    addressInput.classList.add('valid');
+                    addressWarning.classList.remove('shown');
+                    addressValid = true;
+                }   
+            })
+        })
+
+        const addressWarning = document.createElement('p'); 
+        addressWarning.className = 'address-warning';
+        addressWarning.textContent = 'Your address must contain 3 words(minimum 5 symbols each)';
+        formContainer.append(addressWarning);
         
         const emailInput = document.createElement('input');
         emailInput.className = 'form__email';
@@ -68,6 +129,11 @@ class Modal {
         emailInput.placeholder = 'E-mail: ex. john2022@yahoo.com';
         emailInput.required = true;
         formContainer.append(emailInput);
+
+        const emailWarning = document.createElement('p'); 
+        emailWarning.className = 'email-warning';
+        emailWarning.textContent = 'Please enter valid email';
+        formContainer.append(emailWarning);
 
         const cardTitle = document.createElement('h3');
         cardTitle.className = 'form__card-title';
@@ -85,10 +151,31 @@ class Modal {
 
         const cardNumberInput = document.createElement('input');
         cardNumberInput.id = 'card-number';
-        cardNumberInput.type = 'number';
-        cardNumberInput.pattern = '[0-9]{16}';
+        cardNumberInput.type = 'text';
+        cardNumberInput.maxLength = 16;
+        cardNumberInput.pattern = "^[0-9\\s.\\/-]{16}$";
         cardNumberInput.required = true;
         cardInfo.append(cardNumberInput);
+
+        const cardNumberWarning = document.createElement('p'); 
+        cardNumberWarning.className = 'card-number-warning';
+        cardNumberWarning.textContent = 'Please enter valid card number';
+        cardInfo.append(cardNumberWarning);
+
+        // cardNumberInput.addEventListener('input', () => {
+        //     if(cardNumberInput.value.startsWith('4')) {
+        //         cardNumberInput.style.background = `url('../../assets/svg/visa.svg')`;
+        //         cardNumberInput.style.backgroundPosition = 'left 10px center';
+        //         cardNumberInput.style.backgroundSize = 'contain';
+        //         cardNumberInput.style.backgroundRepeat = 'no-repeat';
+        //     }
+        //     else if(cardNumberInput.value.startsWith('5')) {
+        //         cardNumberInput.style.background = `url('/assets/svg/mastercard.svg')`;
+        //         cardNumberInput.style.backgroundPosition = 'left 10px center';
+        //         cardNumberInput.style.backgroundSize = 'contain';
+        //         cardNumberInput.style.backgroundRepeat = 'no-repeat';
+        //     }
+        // })
 
         const cardsAdd = document.createElement('div');
         cardsAdd.className = 'card-info__add';
@@ -101,10 +188,15 @@ class Modal {
 
         const cardValidInput = document.createElement('input');
         cardValidInput.id = 'valid';
-        cardValidInput.type = 'number';
-        cardValidInput.pattern = '(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])'; // возможно
+        cardValidInput.type = 'text';
+        cardValidInput.maxLength = 5;
+        cardValidInput.pattern = "/^(0[1-9]|1[0-2])\/\d{2}$/"; // возможно
         cardValidInput.required = true;
         cardsAdd.append(cardValidInput);
+
+        cardValidInput.addEventListener("input", () => {
+
+        })
 
         const labelCvv = document.createElement('label');
         labelCvv.htmlFor = 'cvv';
@@ -113,15 +205,17 @@ class Modal {
 
         const cardCvvInput = document.createElement('input');
         cardCvvInput.id = 'cvv';
-        cardCvvInput.type = 'number';
-        cardCvvInput.pattern = '[0-9]{3}';
+        cardCvvInput.type = 'text';
+        cardCvvInput.maxLength = 3;
+        cardCvvInput.pattern = '^[0-9\\s.\\/-]{3}$';
         cardCvvInput.required = true;
         cardsAdd.append(cardCvvInput);
 
         const submitButton = document.createElement('button');
         submitButton.type = 'submit';
-        submitButton.className = 'form__button button_submit';
+        submitButton.className = 'form__button button_submit inactive';
         submitButton.textContent = 'Submit';
+        submitButton.disabled = true;
         formContainer.append(submitButton);
 
         window.addEventListener('click', (e) => {
